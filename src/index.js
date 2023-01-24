2d7d4e06d1e1e7058385be814e76b49574f2595c
unknown <home_dv@mail.ru>
2023-01-24 09:08:39 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix === undefined) return [];
+  matrix.forEach((e, i) => {
+    if(i % 2 !== 0) e.reverse();
+  });
+  return [].concat(...matrix);
 }
